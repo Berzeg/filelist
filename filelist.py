029b93850b7faf42e45bf7a53f13143d46d9600b
Hashem <h9.11.2s@gmail.com>
2016-02-16 00:20:18 -0500

    Create method to list all files in directory
    So, yeah. I wrote and tested this script that traverses a directory and
    returns a list of all files in there. But wait; there's more! You can
    designate an ignore file, and any files that match with an ignore rule
    will not be in the output!!!

---
+++ filelist.py
@@ -0,0 +1,84 @@
+__author__ = "Hashem Berzeg"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__email__ = "h9.11.2s@gmail.com"
+
+import os
+import re
+import fnmatch
+
+def filelist(absolute_dir, absolute_filenames=True, ignore_file=".pyignore"):
+    """ Get all files in a directory that aren't to be ignored
+
+    Compiles ignore rules (in shell-style format) as it traverses down a
+    directory, and returns a list of absolute filepaths that pass the
+    filter (the files that shouldn't be ignored are returned).
+
+    absolute_dir - The absolute path of the root directory at which to
+        start the walk. All .pyignore files in addresses above this one
+        in the filetree will be ignored.
+    absolute_filenames - Whether the returned file paths should be
+        relative to the provided root directory (absolute_dir), or
+        whether they should be absolute paths.
+    ignore_file - The standard name of all files that contain ignore
+        rules in shell-style format.
+    """
+    leaves = _filelist(absolute_dir, ".", [], ignore_file)
+
+    if not absolute_filenames:
+        leaves = _relative_filelist(absolute_dir, leaves)
+
+    return leaves
+
+def _filelist(absolute_path, relative_path, ignore_rules, ignore_file):
+    ignore_rules = ignore_rules.copy()
+    pwd_nodes = os.listdir(absolute_path)
+
+    # Add new ignore rules, if any exist
+    if ignore_file in pwd_nodes:
+        ignore_filepath = os.path.join(absolute_path, ignore_file)
+        ignore_file_handler = open(ignore_filepath, 'r')
+        new_rules = ignore_file_handler.read().splitlines()
+
+        ignore_rules.extend(new_rules)
+
+    pwd_files = []
+
+    for node in pwd_nodes:
+        absolute_node_path = os.path.join(absolute_path, node)
+        relative_node_path = os.path.join(relative_path, node)
+
+        if _should_ignore(relative_node_path, ignore_rules):
+            continue
+
+        if os.path.isdir(node):
+            child_files = _filelist(absolute_node_path, relative_node_path, ignore_rules, ignore_file)
+            pwd_files.extend(child_files)
+        else:
+            pwd_files.append(absolute_node_path)
+
+    return pwd_files
+
+def _relative_filelist(root_dir, filelist):
+    new_filelist = []
+
+    for f in filelist:
+        start_index = f.find(root_dir)
+        end_index = start_index + len(root_dir)
+        new_filelist.append('.' + f[end_index:])
+
+    return new_filelist
+
+def _should_ignore(filepath, ignore_rules):
+    for rule in ignore_rules:
+        # Megahack: fnmatch doesn't know how to match certain filepaths.
+        # e.g. pattern = ".git", path_to_test = "a/.git", match => False
+        #      pattern = "*/.git", path_to_test = "a/.git", match => True
+        lax_rule = os.path.join("*", rule)
+        lax_expression = fnmatch.translate(lax_rule)
+        lax_re = re.compile(lax_expression)
+
+        if lax_re.match(filepath):
+            return True
+
+    return False
